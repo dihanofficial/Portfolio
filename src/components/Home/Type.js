1e4f39068d075da4a0d8531b5b842d0d6e4668f7
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-20 18:40:00 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Web Site Developer",
+          "Bots Developer",
+          "Music Lover",
+          "Youtuber",
         ],
         autoStart: true,
         loop: true,
